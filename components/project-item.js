429a8564040d41b30509df1ca3892670f4c82267
Yujin Yi <yujin94@gmail.com>
2022-08-11 22:34:17 +0900

    add icons

--- components/project-item.js
+++ components/project-item.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 //project.js에서 aProject가 data.
 export default function ProjectItem({data}){
@@ -12,6 +13,10 @@ export default function ProjectItem({data}){
 
     const tags = data.properties.Tags.multi_select
 
+    const days = data.properties.Days.formula.string
+
+    const pageId = data.id
+
     let imgSrc;
 
     if(data.icon == null || data.icon.type == "emoji"){
@@ -23,24 +28,42 @@ export default function ProjectItem({data}){
     }
 
     let cateColor;
+    let icon;
 
-    if(category == "Study"){
-        cateColor = "Study"
-    }else if(category == "Read"){
-        cateColor = "Read"
-    }else if(category == "Watch"){
-        cateColor = "Watch"
-    }else if(category == "Experience"){
-        cateColor = "Experience"
-    }else if(category == "Review"){
-        cateColor = "Review"
-    }else if(category == "Think"){
-        cateColor = "Think"
+    const icons = {
+        "earth" : "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/96a7fbd2-b822-4a8f-8728-a558faaecc00/Earth_globe_asia-australia.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220811%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220811T133110Z&X-Amz-Expires=86400&X-Amz-Signature=0a401806075adb0d4632370401bcd28f28a721c1cf9204bf3c62636768f0f020&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Earth%2520globe%2520asia-australia.png%22&x-id=GetObject",
+        "rock" : "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/41dae367-9f86-496e-a65c-07c249a16012/0_-_Default.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220811%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220811T132153Z&X-Amz-Expires=86400&X-Amz-Signature=4d6a10d14aa063b42b0d22f869348bfc2c8b584c064418ef27afa165fc12163e&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%220%2520-%2520Default.png%22&x-id=GetObject",
+        "bolt" :"https://s3.us-west-2.amazonaws.com/secure.notion-static.com/5248efa3-3b07-4001-8eb0-05b9b507ef9f/High_voltage_sign.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220811%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220811T132155Z&X-Amz-Expires=86400&X-Amz-Signature=0343111c7ee14987a263b3d195a0d0bbee8dc81ede87455ea894bef1fddd2db8&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22High%2520voltage%2520sign.png%22&x-id=GetObject",
+        "clover" :"https://s3.us-west-2.amazonaws.com/secure.notion-static.com/dfb5d568-e027-487f-860a-668c017065fd/Shamrock.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220811%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220811T132158Z&X-Amz-Expires=86400&X-Amz-Signature=8c93042d146dc4db51e09817c4dafc050420498b1f4c570a6a753544ea1cc084&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Shamrock.png%22&x-id=GetObject",
+        "sun" :"https://s3.us-west-2.amazonaws.com/secure.notion-static.com/5af62f57-c9b8-4fc8-b246-893d21d4b44e/Sun_with_rays.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220811%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220811T132201Z&X-Amz-Expires=86400&X-Amz-Signature=8cee6c80b7cb92b1f0cef3e65681b6608c326e850a6eab2b1c6ff317e8518f10&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Sun%2520with%2520rays.png%22&x-id=GetObject",
+        "cloud" :"https://s3.us-west-2.amazonaws.com/secure.notion-static.com/fd1866b5-f7d7-4257-852d-96147799ff9d/Thought_balloon.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220811%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220811T132204Z&X-Amz-Expires=86400&X-Amz-Signature=56f899d44573466a7b6ece0bdcc70c6579e8303b3a95c600140ead51d523419c&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Thought%2520balloon.png%22&x-id=GetObject",
+        "riceball" : "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/0f777ab6-128d-4fca-9308-01257a59d808/Rice_ball.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220811%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220811T133114Z&X-Amz-Expires=86400&X-Amz-Signature=7d1e7033790a4958a119ddceaf243d1dfe4d41fb06080a4037eb4b65e7c75621&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Rice%2520ball.png%22&x-id=GetObject",
+        "butterfly" : "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/219f5c6e-b2ea-496b-8db9-8bb7c886c32b/Butterfly.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220811%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220811T133118Z&X-Amz-Expires=86400&X-Amz-Signature=8ef0c7797c5042f4419b9c81a69f2a8fd0fdc2cf4f4c64180f01414ec81cbef6&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Butterfly.png%22&x-id=GetObject"
     }
 
+    if(category == "Study"){
+        cateColor = "Study",
+        icon = icons['earth']
+    }else if(category == "Read"){
+        cateColor = "Read",
+        icon = icons['butterfly']
+    }else if(category == "Watch"){
+        cateColor = "Watch",
+        icon = icons['bolt']
+    }else if(category == "Experience"){
+        cateColor = "Experience",
+        icon = icons['clover']
+    }else if(category == "Review"){
+        cateColor = "Review",
+        icon = icons['sun']
+    }else if(category == "Think"){
+        cateColor = "Think",
+        icon = icons['cloud']
+    }
 
     return(
         <>
+        <Link href={`/projects/${pageId}`}>
         <div className="projectItem bg-white mb-4 rounded-3xl drop-shadow-sm">
 
             <div className='imgContainer'>
@@ -48,9 +71,10 @@ export default function ProjectItem({data}){
             </div>
             
             <div className={cateColor}>
-                <div className='textContainer'>
+                <div className='textContainer pt-4 ml-4 mr-4 text-white'>
                     <div className='titles flex flex-row'>
-                        <p className='mr-2'>{category}</p>
+                        <Image src={icon} width="16px" height="16px" objectFit='contain'></Image>
+                        <p className='ml-2 mr-2'>{category}</p>
                             {tags.map((aTag) => (
                                 <p className='mr-2 opacity-80' key={aTag.id}>{"// " + aTag.name}</p>
                             )
@@ -59,10 +83,14 @@ export default function ProjectItem({data}){
 
                     <h1 className='projectTitle'>{projectTitle}</h1>
 
-                    <h1 className='date opacity-80'>{date}</h1>
+                    <div className='dates text-xs flex flex-row pb-4'>
+                        <p className='mr-2'>{days}</p>
+                        <p className=''>{date}</p>
+                    </div>
                 </div>
             </div>            
         </div>
+        </Link>
         </>
     )
 }
--- pages/projects.js
+++ pages/projects.js
@@ -1,15 +1,18 @@
 import Layout from "../components/layout"
 import {TOKEN, DATABASE_ID} from "../config"
 import ProjectItem from "../components/project-item";
+import { data } from "autoprefixer";
 
 //projects를 넘게 받고, 파싱해서 쓰기
 export default function Projects({projects}){
+    console.log(projects)
     // console.log(projects.results)
     return(
         <>
             <Layout>
                 <div className="flex flex-col place-items-center">
                     <h1 className="mb-4 font-bold">Total {projects.results.length}</h1>
+                    {/* <h1 className="mb-4 font-bold">Total {results.length}</h1> */}
 
                     {projects.results.map((aProject)=>(
                         <ProjectItem key={aProject.id} data={aProject}/> //1:55:54
@@ -39,15 +42,18 @@ export async function getServerSideProps() {
                     "direction" : "descending"
                 }
             ],
-            page_size: 100
+            // start_cursor: projects.next_cursor
         })
     };
     
     //데이터가 받아질 때까지 기다리기
-    const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options)
+    let res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options)
 
     //결과를 json으로 만들어봄
-    const projects = await res.json()
+    let projects = await res.json()
+
+
+
 
   return {
     props: {projects}, // will be passed to the page component as props
